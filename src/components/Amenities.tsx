b4fdb886ddeed3aa19cdc9e52a327ecbe1febb6b
Stro <stro@Stros-Mac-mini.local>
2026-06-16 21:49:40 -0600

    fix: remove duplicate cake pop kitchen logo and update rusty bucket logo

--- src/components/Amenities.tsx
+++ src/components/Amenities.tsx
@@ -95,7 +95,6 @@ export default function Amenities() {
               { src: '/krispykrunchy.png', alt: 'Krispy Krunchy Chicken' },
               { src: '/bluebunny.png', alt: 'Blue Bunny Ice Cream' },
               { src: '/partner_logo_1.png', alt: 'Featured Logo' },
-              { src: '/partner_logo_2.png', alt: 'Featured Logo' },
               { src: '/partner_logo_3.png', alt: 'Featured Logo' },
               { src: '/rustybucket.png', alt: 'The Rusty Bucket' },
               { src: '/wyomingfudge.png', alt: 'Wyoming Fudge Co' },
